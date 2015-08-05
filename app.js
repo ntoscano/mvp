1785019aed26c6cab80ea8687e6d684872121341
ntoscano <Nicolas.Toscano@gmail.com>
2015-08-04 17:28:25 -0700

    version 2.0: user can now customize the lines that get added to the webpage

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var speedReader = function(){
+var speedReader = function(red, green, blue, opacity, width, percent){
  var sheets = document.styleSheets;
  var sheet = sheets[sheets.length - 1];
- sheet.insertRule(" p:before { content:''; border-left:1px solid red; border-right:1px solid red; position: absolute; width: 80%; margin-left: 10%; height: 100%; display: inline-block; }", 0);
+ var dist = 100 - (percent * 2);
+ sheet.insertRule(" p:before { content:''; border-left:"+width+"px solid rgba("+red+", "+green+", "+blue+", "+opacity+"); border-right:"+width+"px solid rgba("+red+", "+green+", "+blue+", "+opacity+"); position: absolute; width: "+dist+"%; margin-left: "+percent+"%; height: 100%; display: inline-block; }", 0);
  sheet.insertRule(" p { display: block; position: relative }", 1);
 };
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if(request.greeting === "hello") {speedReader()}
+    if(request.sr === "sr") {speedReader(request.red, request.green, request.blue, request.opacity, request.width, request.percent)}
   });
--- popup.js
+++ popup.js
@@ -1,9 +1,27 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+angular.module('color', []).controller('colorController', function($scope) {
+  console.log('controller');
+  $scope.red = 255;
+  $scope.green = 0;
+  $scope.blue = 0;
+  $scope.opacity = 1;
+  $scope.width = 1;
+  $scope.percent = 10;
   $( document ).ready(function() {
-    $('body').on("click", function(){ 
-      console.log('clicked');
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-      });
+    console.log('ready');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      console.log('conected');
+      $('#Btn').on("click", function(){ 
+        console.log('clicked');
+        chrome.tabs.sendMessage(tabs[0].id, {
+          sr : 'sr',
+          red: $scope.red,
+          green: $scope.green,
+          blue: $scope.blue,
+          opacity: $scope.opacity,
+          width: $scope.width,
+          percent: $scope.percent
+         }, function(response) {});
+      })
     })
   })
 });
